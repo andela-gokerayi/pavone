eb54fd7c23d94f9fa13dfc78458e80c023c7b10d
Gbolahan Okerayi <gbolahan.okerayi@andela.co>
2015-05-15 15:06:40 +0100

    Edited Procfile and added whitenoise

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn pavone.wsgi --log-file -
+web: gunicorn library.wsgi 
--- pavone/wsgi.py
+++ pavone/wsgi.py
@@ -12,8 +12,10 @@ import envvars
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", envvars.get("DJANGO_SETTINGS_MODULE"))
 
 application = get_wsgi_application()
 application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
